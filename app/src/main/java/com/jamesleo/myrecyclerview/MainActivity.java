1b16ab080cc5fcd5d5a22e3fba24185dd2f53b8d
leo-gaming <53498600+leo-gaming@users.noreply.github.com>
2020-09-18 21:10:26 +0700

    Initial commit

---
+++ app/src/main/java/com/jamesleo/myrecyclerview/MainActivity.java
@@ -0,0 +1,58 @@
+package com.jamesleo.myrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.android.myrecyclerview.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+	private RecyclerView rvHeroes;
+	private ArrayList<Hero> list = new ArrayList<>();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		rvHeroes = findViewById(R.id.rv_heroes);
+		rvHeroes.setHasFixedSize(true);
+
+		list.addAll(HeroesData.getListData());
+		showRecyclerList();
+	}
+	private void showRecyclerList(){
+		rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+		ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+		rvHeroes.setAdapter(listHeroAdapter);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		setMode(item.getItemId());
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void setMode(int selectedMode) {
+		switch (selectedMode){
+			case R.id.action_list:
+				showRecyclerList();
+				break;
+			case  R.id.action_grid:
+				break;
+			case R.id.action_cardview:
+				break;
+		}
+	}
+}

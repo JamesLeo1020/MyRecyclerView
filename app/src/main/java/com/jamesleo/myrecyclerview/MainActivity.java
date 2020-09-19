f85036248b2d81b6222c668c0371c2526a70db79
james hestu satria <53498600+leo-chan1020@users.noreply.github.com>
2020-09-19 20:48:39 +0700

    Update MainActivity.java

--- app/src/main/java/com/jamesleo/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/jamesleo/myrecyclerview/MainActivity.java
@@ -1,13 +1,13 @@
 package com.jamesleo.myrecyclerview;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.android.myrecyclerview.R;
 
@@ -28,16 +28,25 @@ public class MainActivity extends AppCompatActivity {
 		list.addAll(HeroesData.getListData());
 		showRecyclerList();
 	}
-	private void showRecyclerList(){
+
+	private void showRecyclerList() {
 		rvHeroes.setLayoutManager(new LinearLayoutManager(this));
 		ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
 		rvHeroes.setAdapter(listHeroAdapter);
 	}
+
+	private void showRecyclerGrid() {
+		rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
+		GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+		rvHeroes.setAdapter(gridHeroAdapter);
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu_main, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		setMode(item.getItemId());
@@ -45,11 +54,12 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	private void setMode(int selectedMode) {
-		switch (selectedMode){
+		switch (selectedMode) {
 			case R.id.action_list:
 				showRecyclerList();
 				break;
-			case  R.id.action_grid:
+			case R.id.action_grid:
+				showRecyclerGrid();
 				break;
 			case R.id.action_cardview:
 				break;
